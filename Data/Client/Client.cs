3cc6f4da76b859aba592fe98843cfdae76271d12
ZDubeau <zdubeau@dubeaul.com>
2022-03-21 14:58:23 +0100

    Add Client class

---
+++ Data/Client/Client.cs
@@ -0,0 +1,15 @@
+    
+public class Client {
+
+    public long Id { get; set; }
+
+    public string Lastname { get; set; }
+
+    public string Firstname { get; set; }
+
+    public ClientType ClientTypes { get; set; }
+
+    public ICollection<Factor> Factors { get; set; }
+
+    public ICollection<Coordinate> ClientCoordinate { get; set; }
+}
