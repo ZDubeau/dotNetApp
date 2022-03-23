71d6a371fbd97e4e5d16b4d6ec0a056790ae19b7
ZDubeau <zdubeau@dubeaul.com>
2022-03-23 10:10:26 +0100

    Create folder ENUM & move enum inside

--- Data/Enum/ClientType.cs
+++ Data/Enum/ClientType.cs
@@ -1,5 +1,7 @@
 
+#pragma warning disable CA1050 // Declare types in namespaces
 public enum ClientType
+#pragma warning restore CA1050 // Declare types in namespaces
 {
     Recurrent,
     OneShot
